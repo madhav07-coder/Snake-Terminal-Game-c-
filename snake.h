aa024fdb2fbed64bd7b52ae443102f53fa7325f1
madhav07-coder <madhavmanishthesiya@gmail.com>
2025-02-13 21:58:57 +0530

    Update snake.h

--- snake.h
+++ snake.h
@@ -8,12 +8,13 @@ using namespace std;
 class Snake 
 {
 private:
+    int gridSize;
     deque<pair<int, int>> body; // Snake's body stored as a deque of coordinates
     char direction;     // Current direction ('U', 'D', 'L', 'R')
     bool isGrowing;      // flag to show that snake is growing     
 
 public:
-    Snake(int startX, int startY);   // Constructor
+    Snake(int startX, int startY,int gridSize);   // Constructor
     void move();    // sap ne move kare
     void grow();    // sap ne grow kare
     bool checkSelfCollision() const;   // sap nu potani sathe collison check kare.
